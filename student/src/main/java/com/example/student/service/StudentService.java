2925ce913604ffea27dfba8756ca547bf70185c6
qudeo <korapadug@gmail.com>
2024-01-15 15:40:28 +0900

    student

---
+++ student/src/main/java/com/example/student/service/StudentService.java
@@ -0,0 +1,34 @@
+package com.example.student.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.student.dto.StudentDTO;
+import com.example.student.repository.StudentRepository;
+
+@Service
+public class StudentService {
+	@Autowired
+	private StudentRepository studentRepository;
+
+	public void save(StudentDTO studentDTO) {
+		System.out.println("StudentService.save()");
+		System.out.println("StudentDTO = " + studentDTO);
+		studentRepository.save(studentDTO);
+		
+	}
+
+	public List<StudentDTO> findAll() {
+		return studentRepository.findAll();
+	}
+
+	public StudentDTO findById(Long id) {
+		// TODO Auto-generated method stub
+		return studentRepository.findById(id);
+	}
+
+	
+	
+}
